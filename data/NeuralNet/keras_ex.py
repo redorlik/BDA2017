7c7fba1da633f234d28e80836939edf84f59fdf6
Anders Lehmann <redorlik@gmail.com>
2017-11-23 11:26:48 +0100

    add NN image recognition example

--- data/NeuralNet/keras_ex.py
+++ data/NeuralNet/keras_ex.py
@@ -2,30 +2,43 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
+from keras.datasets import mnist
 
-# Generate dummy data
-import numpy as np
-x_train = np.random.random((1000, 20))
-y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
-x_test = np.random.random((100, 20))
-y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
+
+x_train = x_train.reshape(x_train.shape[0],28*28)
+x_test = x_test.reshape(x_test.shape[0],28*28)
+
+x_train = x_train.astype('float32')
+x_train = x_train/255.
+
+x_test = x_test.astype('float32')
+x_test = x_test/255.
+
+num_classes = 10
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-# Dense(64) is a fully-connected layer with 64 hidden units.
-# in the first layer, you must specify the expected input data shape:
-# here, 20-dimensional vectors.
-model.add(Dense(64, activation='relu', input_dim=20))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
+
+model.add(Dense(512, activation='relu', input_shape=(28*28,)))
+#model.add(Dropout(0.1))
+hid = Dense(512, activation='relu')
+model.add(hid)
+#model.add(Dropout(0.1))
+out = Dense(10, activation='softmax')
+model.add(out)
+print(model.summary())
 
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
-
+print(x_train.shape)
 model.fit(x_train, y_train,
           epochs=20,
-          batch_size=128)
+          batch_size=128,
+          verbose=1,
+                    validation_data=(x_test, y_test))
+
 score = model.evaluate(x_test, y_test, batch_size=128)
